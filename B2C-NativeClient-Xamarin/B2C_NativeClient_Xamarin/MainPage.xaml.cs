3b163136e52f06a35709c5f2937faddcd56a41ad
Kai Walter <kai.walter@outlook.de>
2016-04-05 20:47:29 +0200

    simple form running; update to Xamarin.Forms.2.2.0.4-pre1

---
+++ B2C-NativeClient-Xamarin/B2C_NativeClient_Xamarin/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace B2C_NativeClient_Xamarin
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+        void OnSignIn(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnSignUp(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
