eeb4fdaafe4b5b45014ff2e0e2e133337e35ce0b
Kai Walter <kai.walter@outlook.de>
2016-04-05 21:14:46 +0200

    added ADAL v4 library

---
+++ B2C-NativeClient-Xamarin/B2C_NativeClient_Xamarin/Globals.cs
@@ -0,0 +1,18 @@
+﻿namespace B2C_NativeClient_Xamarin
+{
+    public static class Globals
+    {
+        // TODO: Replace these with your own configuration values
+        public static string tenant = "[Tenant name of your Azure AD B2C e.g. contoso.onmicrosoft.com]";
+        public static string clientId = "[Client ID of B2C App created in Portal e.g. ac12a187-c653-4236-970c-65807dc4413d]";
+        public static string signInPolicy = "[Sign in policy name as created in Portal e.g. B2C_1_todolist_signin]";
+        public static string signUpPolicy = "[Sign up policy name as created in Portal e.g. B2C_1_todolist_signup]";
+        public static string editProfilePolicy = "[Edit profile policy name as created in Portal e.g. B2C_1_todolist_profile]";
+
+        public static string aadInstance = "https://login.microsoftonline.com/";
+        public static string redirectUri = "urn:ietf:wg:oauth:2.0:oob";
+
+        public static string todoListBaseAddress = "[Base URL of ToDo List of service published to Azure App Service e.g. https://my-todolist-service.azurewebsites.net]";
+
+    }
+}
--- B2C-NativeClient-Xamarin/B2C_NativeClient_Xamarin/MainPage.xaml.cs
+++ B2C-NativeClient-Xamarin/B2C_NativeClient_Xamarin/MainPage.xaml.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Experimental.IdentityModel.Clients.ActiveDirectory;
 using Xamarin.Forms;
+using System.Net.Http;
 
 namespace B2C_NativeClient_Xamarin
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage ()
+        private HttpClient httpClient = new HttpClient();
+        private AuthenticationContext authContext = null;
+
+        public MainPage ()
 		{
 			InitializeComponent ();
-		}
+
+            authContext = new AuthenticationContext(Globals.aadInstance + Globals.tenant);
+        }
 
         void OnSignIn(object sender, EventArgs e)
         {
