09aeba6b5b9da3d1240bbfd086d9e79ff0bf3ebd
Kai Walter <kai.walter@outlook.de>
2016-04-05 19:03:38 +0200

    Add project files.

---
+++ B2C-NativeClient-Xamarin/B2C_NativeClient_Xamarin.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace B2C_NativeClient_Xamarin.Droid
+{
+	[Activity (Label = "B2C_NativeClient_Xamarin", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new B2C_NativeClient_Xamarin.App ());
+		}
+	}
+}
+
